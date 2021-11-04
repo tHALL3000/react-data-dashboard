c73459649ca617b2903a5dd193c90b09094cbd39
tHALL3000 <tiffanyhall.co@gmail.com>
2021-11-04 15:02:21 -0600

    fake data is rendering

--- thall3000-dashboard/src/components/Chart.js
+++ thall3000-dashboard/src/components/Chart.js
@@ -2,17 +2,57 @@ import React, { Component } from "react";
 import { Bar, Line } from "react-chartjs-2";
 
 class Chart extends Component {
-    render() {
-        return (
-            <div className= "chart">
-        <Bar
-            data={data}
-            width={100}
-            height={50}
-            options={{ maintainAspectRatio= false }}
-
-        />
-            </div>
-        )
-    }
+	constructor(props) {
+		super(
+			props
+		);
+		this.state =
+			{
+				chartData: {
+					labels: [
+						0,
+						1000,
+						2000,
+						3000,
+						4000,
+						5000,
+					],
+					datasets: [
+						{
+							labels: "Bytes Distribution",
+							data: [
+								2044,
+								1980,
+								3489,
+								4400,
+							],
+							backgroundColor: [
+								"rgba(214, 149, 239, 0.7)",
+								"rgba(255, 124, 216, 0.7)",
+								"rgba(255, 66, 193, 0.7)",
+								"rgba(102, 150, 253, 0.7)",
+								"rgba(107, 65, 232, 0.7)",
+							],
+						},
+					],
+				},
+			};
+	}
+	render() {
+		return (
+			<div className="chart">
+				<Bar
+					data={
+						this
+							.state
+							.chartData
+					}
+					options={{
+						maintainAspectRatio: false,
+					}}
+				/>
+			</div>
+		);
+	}
 }
+export default Chart;
