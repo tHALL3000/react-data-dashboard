0dddad6b3dd64802fc86306e01faf82fdd455547
tHALL3000 <tiffanyhall.co@gmail.com>
2021-11-04 14:15:38 -0600

    chart component added

--- thall3000-dashboard/src/components/Chart.js
+++ thall3000-dashboard/src/components/Chart.js
@@ -1,1 +1,18 @@
 import React, { Component } from "react";
+import { Bar, Line } from "react-chartjs-2";
+
+class Chart extends Component {
+    render() {
+        return (
+            <div className= "chart">
+        <Bar
+            data={data}
+            width={100}
+            height={50}
+            options={{ maintainAspectRatio= false }}
+
+        />
+            </div>
+        )
+    }
+}
